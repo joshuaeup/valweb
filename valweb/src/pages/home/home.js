0c3018b0d332df665ab01acf418a28f2a4537ea0
Joshua Proctor <joshuaproctor@joshuas-mbp.home>
2019-06-29 17:41:00 -0400

    added grid basic front end code

--- valweb/src/pages/home/home.js
+++ valweb/src/pages/home/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import Banner from "../../components/banner";
+import Grid from "../../components/grid";
 require ("./home.css");
 
 class home extends React.Component {
@@ -16,6 +17,7 @@ class home extends React.Component {
             <div id="homeMainContainer" className="container-fluid">
                 <Navbar />
                 <Banner />
+                <Grid />
             </div>
         )
     }
